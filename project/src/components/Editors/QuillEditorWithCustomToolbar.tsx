b7decebde87763b9be2a332ca6cba002095bab35
Lóan Burger <loan.burger@gmail.com>
2022-09-17 14:37:19 +1200

    Addd HTML Link Option

--- project/src/components/Editors/QuillEditorWithCustomToolbar.tsx
+++ project/src/components/Editors/QuillEditorWithCustomToolbar.tsx
@@ -6,21 +6,37 @@ import {
   ToggleButton,
   Typography,
 } from '@mui/material';
-import { useState } from 'react';
-import ReactQuill from 'react-quill';
+import { useCallback, useState } from 'react';
+import ReactQuill, { UnprivilegedEditor } from 'react-quill';
 import DOMPurify from 'dompurify';
 
 import FormatBoldIcon from '@mui/icons-material/FormatBold';
 import FormatItalicIcon from '@mui/icons-material/FormatItalic';
 import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
+import { DeltaStatic, Sources } from 'quill';
 
 type QuillState = {
   editorHtml: string;
 };
 
 /*
+ * Quill editor formats
+ * See https://quilljs.com/docs/formats/
+ */
+const formats = [
+  'header',
+  'bold',
+  'italic',
+  'underline',
+  'list',
+  'indent',
+  `link`,
+];
+
+/*
  * Quill modules to attach to editor
  * See https://quilljs.com/docs/modules/ for complete options
+ * Also see https://quilljs.com/docs/modules/toolbar/#handlers for handlers
  */
 const modules = {
   toolbar: {
@@ -29,13 +45,8 @@ const modules = {
 };
 
 /*
- * Quill editor formats
- * See https://quilljs.com/docs/formats/
- */
-const formats = ['header', 'bold', 'italic', 'underline', 'list', 'indent'];
-
-/*
  * CustomToolbar Component
+ * Also see: https://quilljs.com/docs/modules/toolbar/
  */
 const CustomToolbar = () => (
   <div id="toolbar">
@@ -46,6 +57,15 @@ const CustomToolbar = () => (
         <option value="3" label="Heading 3" aria-label="Heading 3" />
         <option value="" label="Normal text" aria-label="Normal text" />
       </select>
+      <IconButton className="ql-bold" aria-label="Bold">
+        <FormatBoldIcon />
+      </IconButton>
+      <IconButton className="ql-italic" aria-label="Italic">
+        <FormatItalicIcon />
+      </IconButton>
+      <IconButton className="ql-underline" aria-label="Underline">
+        <FormatUnderlinedIcon />
+      </IconButton>
       <span className="ql-formats">
         <ToggleButton
           className="ql-list"
@@ -67,16 +87,8 @@ const CustomToolbar = () => (
           value="+1"
           aria-label="indent right"
         />
+        <ToggleButton className="ql-link" value="link" aria-label="Url" />
       </span>
-      <IconButton className="ql-bold" aria-label="Bold">
-        <FormatBoldIcon />
-      </IconButton>
-      <IconButton className="ql-italic" aria-label="Italic">
-        <FormatItalicIcon />
-      </IconButton>
-      <IconButton className="ql-underline" aria-label="Underline">
-        <FormatUnderlinedIcon />
-      </IconButton>
     </Grid>
   </div>
 );
@@ -87,9 +99,20 @@ const QuillEditorWithCustomToolbar = () => {
       '<p>Nested List</p><ul><li>List1</li><li class="ql-indent-1">Nested List</li></ul><p><br></p><p>Hello World. <strong>This is bold.</strong></p>',
   });
 
-  const handleChange = (html: string) => {
-    setState({ editorHtml: html });
-  };
+  const handleChange = useCallback(
+    (
+      value: string,
+      delta: DeltaStatic,
+      source: Sources,
+      editor: UnprivilegedEditor,
+    ) => {
+      console.log('handleChange delta:', delta);
+      console.log('handleChange source:', source);
+      console.log('handleChange editor:', editor);
+      setState({ editorHtml: value });
+    },
+    [],
+  );
 
   const createMarkup = (html: string) => ({
     __html: DOMPurify.sanitize(html),
